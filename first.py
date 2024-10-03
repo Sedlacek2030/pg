2c98e16425a3e401bed83c0e7f3057ae50056959
Sedlacek2030 <sedlap12@jcu.cz>
2024-10-03 09:02:06 +0000

    Sudy-Lichy

--- first.py
+++ first.py
@@ -1,5 +1,9 @@
 print("Hello world!")
 
-def sudy_nebol_lichy():
-    pass
+def sudy_nebol_lichy(number):
+    if number % 2 == 0:
+        print("Number is even")
+    else:
+        print("Number is odd")
 
+sudy_nebol_lichy()
