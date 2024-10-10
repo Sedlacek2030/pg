0a6316735ae72981e41a7ddc5e3b1d5483a363fc
Sedlacek2030 <sedlap12@jcu.cz>
2024-10-10 10:58:16 +0200

    #

---
+++ pokus2.py
@@ -0,0 +1,39 @@
+# funkce vrati treti prvek ze seznamu pokud ma mene nez 3 prvky, vrati None
+def vrat_treti(seznam):
+    if len(seznam) < 3:
+        return None
+    else:
+        return seznam[2]
+
+# funkce spocita prumer z hodnot v seznamu
+def udelej_prumer(seznam):
+    return sum(seznam)/len(seznam)
+
+# funkce naformatuje retezec, aby vratila text ve formatu:
+# "Jmeno: Jan, Prijmeni: Novak, Vek: 20, Prumerna znamka: 2.5"
+def naformatuj_text(slovnik):
+    jmeno = slovník["Jmeno"]
+    return f"Jmeno:{jmeno}"
+
+
+if __name__ == "__main__":
+    
+    obalka = [9,8,7,6,5]
+    vysledek = udelej_prumer(obalka)
+    print (vysledek)
+    
+    print(vrat_treti([9,8,7,6,5]))      #vrať 7
+    print(udelej_prumer([9,8,7,6,5]))   
+    student = {
+        "jmeno": "Matěj",
+        "prijmeni": "Dvořák",
+        "vek": 21,
+        "znamky": [1, 2, 1, 1, 3, 2]
+    }
+    vysledek = naformatuj_text(student)
+    print(vysledek)
+
+    student["vek"]
+    student["prijmeni"]
+    student["jmeno"]
+    student["znamky"]
