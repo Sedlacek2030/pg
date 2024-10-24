49d91ef03036d4fe8dab024d2e801ac37f6bb8b0
Sedlacek2030 <sedlap12@jcu.cz>
2024-10-24 10:16:38 +0200

    none

--- jaccard.py
+++ jaccard.py
@@ -1,5 +1,10 @@
 def jaccardova_vzdalenost_mnozin(mnozina1, mnozina2):
-    pass
+    mnozina1 = set(mnozina1)
+    mnozina2 = set(mnozina2)
+    intersection = mnozina1.intersection(mnozina2)
+    union = mnozina1.union(mnozina2)
+    jaccardov_index = len (intersection)/len(union)
+    return 1 - jaccardov_index 
 
 if __name__ == "__main__":
     serp1 = ["https://www.seznam.cz", "https://www.google.com", "https://www.jcu.cz", "https://www.czu.cz", "https://www.cvut.cz", "https://www.uk.cz"]
