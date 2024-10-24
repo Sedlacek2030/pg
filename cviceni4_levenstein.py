d5d45aca54ea741e124635f70edb7999e56b9d1a
Sedlacek2030 <sedlap12@jcu.cz>
2024-10-24 10:35:17 +0200

    Update cviceni4_levenstein.py

--- cviceni4_levenstein.py
+++ cviceni4_levenstein.py
@@ -1,5 +1,19 @@
 def levensteinova_vzdalenost(dotaz1, dotaz2):
-    pass
+    m, n = len(dotaz1), len(dotaz2)
+    dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+    for i in range(m + 1):
+        for j in range(n + 1):
+            if i == 0:
+                dp[i][j] = j
+            elif j == 0:
+                dp[i][j] = i
+            elif dotaz1[i - 1] == dotaz2[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1]
+            else:
+                dp[i][j] = 1 + min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1])
+
+    return dp[m][n]
 
 
 if __name__ == "__main__":
