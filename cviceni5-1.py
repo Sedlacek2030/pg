7b235cd75d8b1d8d6317122a2fd79c698d490f78
Sedlacek2030 <sedlap12@jcu.cz>
2024-10-31 10:12:03 +0000

    unknown

--- cviceni5-1.py
+++ cviceni5-1.py
@@ -1,15 +1,32 @@
 import sys
+import csv
 
 def nacti_csv(soubor):
-    pass
+    data = []
+    with open(soubor, "r") as fp:
+        reader = csv.reader(fp)
+        for radek in reader:
+            data.append(radek)
+    return data
 
 
 def spoj_data(*data):
-    pass
+    vysledek={}
+    for d in data:
+        for i,v in enumerate(d):
+            if i==0:
+                continue
+            if v[1] not in vysledek:
+                vysledek[v[1]] = v
+            else:
+                vysledek[v[1]].extend(v)
+    return vysledek
 
 
 def zapis_csv(soubor, data):
-    pass
+    with open(soubor, "w") as fp:
+        pass
+
 
 
 if __name__ == "__main__":
@@ -19,6 +36,9 @@ if __name__ == "__main__":
         csv_data1 = nacti_csv(soubor1)
         csv_data2 = nacti_csv(soubor2)
         vysledna_data = spoj_data(csv_data1, csv_data2)
+        print(vysledna_data)
         zapis_csv(vysledna_data)
-    except Exception:
-        pass
+    except IndexError:
+        print("Zadej dva vstupní CSV soubory")
+    except FileNotFoundError:
+        print("Soubor nenalezen")
