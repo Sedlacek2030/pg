b01779e2e31beb4828feec8fe72a611d45a5b226
Sedlacek2030 <sedlap12@jcu.cz>
2024-11-06 21:17:13 +0000

    If you win, you need not have to explain...If you lose, you should not be there to explain!

--- fifth.py
+++ fifth.py
@@ -13,7 +13,15 @@ def read_header(file_name, header_length):
     Tato funkce načte binární soubor z cesty file_name,
     z něj přečte prvních header_length bytů a ty vrátí pomocí return
     """
-    return b'xxx'
+    try:
+        with open(file_name, 'rb') as f:
+            return f.read(header_length)
+    except FileNotFoundError:
+        print(f"Soubor nenalezen")
+        return None
+    except Exception as e:
+        print(f"Došlo k chybě: {e}")
+        return None
 
 
 def is_jpeg(file_name):
@@ -23,10 +31,12 @@ def is_jpeg(file_name):
     """
     # načti hlavičku souboru
     header = read_header(file_name, len(jpeg_header))
+    if header == jpeg_header:
+        return True
+    else:
+        return False
 
     # vyhodnoť zda je soubor jpeg
-
-    return False
 
 
 def is_gif(file_name):
@@ -35,7 +45,13 @@ def is_gif(file_name):
     tu srovná s definovanými hlavičkami v proměnných gif_header1 a gif_header2
     """
     # vyhodnoť zda je soubor gif
-    return False
+    header1 = read_header(file_name, len(gif_header1))
+    header2 = read_header(file_name, len(gif_header2))
+    if header1 == gif_header1 or header2 == gif_header2:
+        return True
+    else:
+        return False
+    
 
 
 def is_png(file_name):
@@ -44,7 +60,11 @@ def is_png(file_name):
     tu srovná s definovanou hlavičkou v proměnné png_header
     """
     # vyhodnoť zda je soubor png
-    return False
+    header = read_header(file_name, len(png_header))
+    if header == png_header:
+        return True
+    else:
+        return False
 
 
 def print_file_type(file_name):
@@ -63,5 +83,10 @@ def print_file_type(file_name):
 
 if __name__ == '__main__':
     # přidej try-except blok, odchyť obecnou vyjímku Exception a vypiš ji
-    file_name = sys.argv[1]
-    print_file_type(file_name)
+    try:
+        file_name = sys.argv[1]
+        print_file_type(file_name)
+    except IndexError:
+        print("Nebyl zadát název souboru.")
+    except Exception as e:
+        print(f"Došlo k neočekávané chybě: {e}")
