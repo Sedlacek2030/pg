806203ea6dc267861bdd2ab56f081c29afcd1706
Sedlacek2030 <sedlap12@jcu.cz>
2024-11-20 19:56:10 +0000

    If you want to shine like sun first you have to burn like it

--- sixth.py
+++ sixth.py
@@ -13,23 +13,25 @@ def download_url_and_get_all_hrefs(url):
     """
     hrefs = []
     response = requests.get(url)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, 'html.parser')
-        for a_tag in soup.find_all("a", href = True):
-            hrefs.append(a_tag["href"])
+    if response.status_code == 200: #status code 200 == úspěšná odpověď
+        soup = BeautifulSoup(response.content, 'html.parser') #rozložení HTML obsahu na manší části, aby počítač "porozuměl" textu
+        for a_tag in soup.find_all("a", href = True): #vyhledání všechodkazů <a href="..."> duh...
+            hrefs.append(a_tag["href"]) #přidání odkazů do seznamu
         else:
             print(f"Chyba: Stavový kód {response.status_code}")
 
-    return hrefs
+    return hrefs #vrátí seznam odkazů
 
 
-if __name__ == "__main__":
+if __name__ == "__main__": #chybí řádek pro zadání URL uživatelem (spuštění př. python sixth.py https://www.jcu.cz/cz/)
     try:
-        url = sys.argv[1]
-        linx = download_url_and_get_all_hrefs(url)
-        print("Nalezené odkazy:")
+        url = sys.argv[1] #získání URL z argumentu příkazového řádku
+        linx = download_url_and_get_all_hrefs(url) #funkce pro vypsání odkazů, obviously
+        print("Nalezené odkazy:") #vypsaání nalezených odkazů
         for link in linx:
             print(link)
     # osetrete potencialni chyby pomoci vetve except
     except Exception as e:
         print(f"Program skoncil chybou: {e}")
+
+#All work done by ChatGPT, maybe
