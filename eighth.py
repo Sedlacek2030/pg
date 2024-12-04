bbf87a4189fbd7f981bf10fcf6c0caee06d97dc8
Sedlacek2030 <sedlap12@jcu.cz>
2024-12-04 17:46:30 +0000

    Think Thousand times before taking a decision, but after taking decison never turn back. Even if you get thousand difficulties!

--- eighth.py
+++ eighth.py
@@ -17,9 +17,9 @@ def test_funkce():
     assert bin_to_dec(10000000) == 128
 
 if __name__ == "__main__":
-    print(bin_to_dec("0")) == 0
-    print (bin_to_dec(1)) == 1
-    print (bin_to_dec("100")) == 4
-    print (bin_to_dec(101)) == 5
-    print (bin_to_dec("010101")) == 21
-    print (bin_to_dec(10000000)) == 128
+    print(bin_to_dec("0"))
+    print (bin_to_dec(1))
+    print (bin_to_dec("100"))
+    print (bin_to_dec(101))
+    print (bin_to_dec("010101"))
+    print (bin_to_dec(10000000))
