38a70ccbf0b3eaebb23bbccc0c8a71a231b83fe0
Sedlacek2030 <sedlap12@jcu.cz>
2024-12-12 09:43:21 +0000

    When diplomacy ends, War begins

--- fourth.py
+++ fourth.py
@@ -93,7 +93,8 @@ if __name__ == "__main__":
     obsazene_pozice = {(2, 2), (8, 2), (3, 3), (5, 4), (8, 3), (8, 8), (6, 3), (1, 4)}
 
     print(je_tah_mozny(pesec, (3, 2), obsazene_pozice))  # True
-    print(je_tah_mozny(pesec, (4, 2), obsazene_pozice))  # False, protože pěšec se nemůže hýbat o dvě pole vpřed (pokud jeho výchozí pozice není v prvním řádku)
+    print(je_tah_mozny(pesec, (4, 2), obsazene_pozice))  # True, při prvním tahu, může pěšec jít o 2 pole dopředu
+    print(je_tah_mozny(pesec, (5, 2), obsazene_pozice))  # False, protože pěšec se nemůže hýbat o tři pole vpřed (pokud jeho výchozí pozice není v prvním řádku). V předchozích verzích tento řádek chyběl, tudíž neprocházely testy
     print(je_tah_mozny(pesec, (1, 2), obsazene_pozice))  # False, protože pěšec nemůže couvat
 
     print(je_tah_mozny(jezdec, (4, 4), obsazene_pozice))  # False, jezdec se pohybuje ve tvaru písmene L (2 pozice jedním směrem, 1 pozice druhým směrem)
@@ -104,7 +105,5 @@ if __name__ == "__main__":
     print(je_tah_mozny(dama, (8, 1), obsazene_pozice))  # False, dámě v cestě stojí jiná figura
     print(je_tah_mozny(dama, (1, 3), obsazene_pozice))  # False, dámě v cestě stojí jiná figura
     print(je_tah_mozny(dama, (3, 8), obsazene_pozice))  # True
-
-
     
     #Assited by ChatGPT
